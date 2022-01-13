cc5023d2fc71bc3b40c34397dce7434cbba3ba33
Judit Orosz <orosz.judit@gmail.com>
2022-01-13 18:41:04 +0100

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent {
    * Hozd létre az alábbi változót.
    * @var phrase {string} - a keresőkifejezés
    */
+  phrase: string = "";
 
 
 
@@ -37,6 +38,9 @@ export class AppComponent {
    * @param event {Event} - az esemény
    * @returns {void}
    */
+   onChangePhrase(event: Event) : void {
+     this.phrase = (event.target as HTMLInputElement).value;
+   }
 
 
 
--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -13,13 +13,16 @@ export class SorterPipe implements PipeTransform {
    */
   transform(value: any[], key: string): any[] {
     // A KÖVETKEZŐ SORT TÁVOLÍTSD EL!!!
-    return value;
+    //return value;
 
     /**
      * FELADAT!
      * Ellenőrzés: ha a value nem tömb vagy nincs megadva a key,
      * térj vissza a value változóval.
      */
+     if (!Array.isArray(value) || !key) {
+      return value;
+    }
 
 
 
@@ -32,6 +35,15 @@ export class SorterPipe implements PipeTransform {
      * 3. Térj vissza a két string localeCompare metódus által visszaadott
      *  összehasonlításának az eredményével.
      */
+    return value.sort((a, b) => {
+      //typeof a[key] === 'number' && typeof b[key] === 'number' ? 
+      //  a[key] - b[key] : a[key].toString().toLowerCase().localeCompare(b[key].toString().toLowerCase());
+      if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+        return a[key] - b[key];
+      } else {
+        return a[key].toString().toLowerCase().localeCompare(b[key].toString().toLowerCase());
+      }
+    })
 
 
   }
